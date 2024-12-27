1ad5bb8d7a9d19a934eb58aa9dcb9035175e786f
Jason Cameron <git@jasoncameron.dev>
2024-12-26 23:25:26 -0500

    Initial commit

---
+++ heartbeat.ps1
@@ -0,0 +1,26 @@
+$time = [DateTimeOffset]::UtcNow.ToUnixTimeSeconds()
+$jsonData = @{
+    branch = "main"
+    category = "coding"
+    cursorpos = 1
+    entity = "welcome.txt"
+    type = "file"
+    lineno = 1
+    lines = 1
+    project = "welcome"
+    time = $time  # Removed quotes to keep it as a number
+    user_agent = "wakatime/v1.102.1 (windows) go1.23.4 vscode/1.94.2 vscode-wakatime/24.6.2"
+} | ConvertTo-Json -Compress
+try {
+    $response = Invoke-RestMethod -Uri "http://localhost:3005/api/heartbeat" `
+                                -Method Post `
+                                -Headers @{
+                                    Authorization = "Bearer Anything"
+                                    "Content-Type" = "application/json"
+                                } `
+                                -Body $jsonData
+    Write-Host "$([char]9830) Heartbeat sent successfully"
+} catch {
+    Write-Host "Error: Heartbeat failed with HTTP status $($_.Exception.Response.StatusCode)"
+    return
+}
